a98ddee1cb346bae11db4f841cdb3053606110ac
nandonakis <git@netconsult.com.au>
2017-06-06 16:12:44 +1000

    Updated postgres scenario1 to illustrate boolean, show not nulls and default attributes.

--- backend/profiles/2_postgres/scenarios.sql
+++ backend/profiles/2_postgres/scenarios.sql
@@ -21,22 +21,20 @@ Date: 2017-05-28 20:03:56
 DROP TABLE IF EXISTS "demo"."scenario1";
 CREATE TABLE "demo"."scenario1" (
 "id" serial primary key,
-"name" varchar(100) COLLATE "default",
+"name" varchar(32) COLLATE "default" DEFAULT ''::character varying NOT NULL,
 "int" int4,
 "currency" numeric(18,2),
 "float" float4,
-"bool" int2,
+"bool" bool,
 "date" date,
 "datetime" timestamp(6),
 "text" text COLLATE "default",
 "email" varchar(100) COLLATE "default",
 "varchar_10" varchar(10) COLLATE "default" ,
-"id_detail" int4,
-"enum" varchar(255) COLLATE "default",
-"default" varchar(255) COLLATE "default" 
+"int4" int4,
+"todo" varchar(255) COLLATE "default",
+"default" varchar(255) COLLATE "default" DEFAULT 'Sydney'::character varying NOT NULL
 )
-WITH (OIDS=FALSE)
-
 ;
 
 -- ----------------------------
